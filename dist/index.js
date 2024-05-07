775d087a1403fbe8f33e515c25c081eda5ecac5d
galusmarcin87 <galusmarcin87@gmail.com>
2024-05-07 13:59:06 +0200

    astro

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,6 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 __exportStar(require("./server/BootServer"), exports);
 __exportStar(require("./types"), exports);
-//# sourceMappingURL=index.js.map
